d10c36e5466a595d033fd08b05e4563fcdec72a4
srimantika <srimantika@gmail.com>
2021-12-07 16:59:09 -0500

    Testing the DayListItem and adding logic for populating available spots dynamically

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,7 +2,21 @@ import React from "react";
 import "components/DayListItem.scss";
 const classNames = require('classnames');
 
+const formatSpots = spots => {
+  if (!spots) {
+    return `no spots remaining`;
+  }
+
+  if (spots === 1) {
+    return `${spots} spot remaining`;
+  }
+
+  return `${spots} spots remaining`;
+}
+
+
 export default function DayListItem(props) {
+  const availabilityMessage = formatSpots(props.spots);
 
   const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
@@ -13,7 +27,7 @@ export default function DayListItem(props) {
     <li className={dayClass}
        onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light"> {availabilityMessage} </h3>
     </li>
   );
 }
