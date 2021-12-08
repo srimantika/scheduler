62cf6c7c518129662130f3cb96290dc7c7b7dd44
srimantika <srimantika@gmail.com>
2021-12-08 09:26:38 -0500

    Building the InterviewerListItem and adding prop detail comment of each component for easy referal later

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import DayListItem from 'components/DayListItem';
 
+
 export default function DayList(props) {
   const schedule = props.days.map(dayObj => {
  
@@ -15,3 +16,8 @@ export default function DayList(props) {
   })
   return <ul>{schedule}</ul>
 }
+
+/*The DayList Component Props: 
+   days:Array a list of day objects (each object includes an id, name, and spots)
+   day:String the currently selected day
+   setDay:Function accepts the name of the day eg. "Monday", "Tuesday"*/
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -31,3 +31,9 @@ export default function DayListItem(props) {
     </li>
   );
 }
+
+/* DayListItem props:
+name:String the name of the day
+spots:Number the number of spots remaining
+selected:Boolean true or false declaring that this day is selected
+setDay:Function accepts the name of the day eg. "Monday", "Tuesday"*/
