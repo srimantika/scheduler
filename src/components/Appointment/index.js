723b0406327115df9070561a27fcc7d932ae2a2f
srimantika <srimantika@gmail.com>
2021-12-08 21:49:40 -0500

    Displaying appointments and the schedule

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import 'components/Appointment/styles.scss';
+import Header from 'components/Appointment/Header';
+import Show from 'components/Appointment/Show';
+import Empty from 'components/Appointment/Empty';
 
 export default function Appointment(props) {
 
 return(
   <article className="appointment">
-    {props.time ? <span> Appointment at {props.time} </span> : <span>No Appointments</span>}
+    <Header time={props.time} />
+    {props.interview ? <Show student ={props.interview.student} interviewer= {props.interview.interviewer}/> : <Empty/>}
   </article>
 )
 
