b0bcd0f0de967b9beb49373562288c773b9515b2
srimantika <srimantika@gmail.com>
2021-12-14 17:49:56 -0500

    Adding validation and tests for student name cannot be blank field

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ export default function Form(props) {
 
   const [student, setStudent] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.value|| null);  
+  const [error, setError] = useState("");
   
     //Helper function to clear all fields
     const reset = () => {
@@ -18,7 +19,11 @@ export default function Form(props) {
       props.onCancel()
     }
 
-    function save() {
+    function validate() {
+      if (student === "" ) {
+        setError("Student name cannot be blank");
+        return;
+      }
       props.onSave(student, interviewer);
     }
     //console.log(props)
@@ -34,19 +39,21 @@ export default function Form(props) {
           onChange={(event) => setStudent(event.target.value)}            
             value={student}
             placeholder="Enter Student Name"
+            data-testid="student-name-input"
       
           /*
             This must be a controlled component
             your code goes here
           */
         />
+      <section className="appointment__validation">{error}</section>
       </form>
       <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={(event) => setInterviewer(event)} />
     </section>
     <section className="appointment__card-right">
       <section className="appointment__actions">
         <Button danger onClick={cancel}>Cancel</Button>
-        <Button confirm onSubmit={event => event.preventDefault()} onClick={save}>Save</Button>
+        <Button confirm onSubmit={event => event.preventDefault()} onClick={validate}>Save</Button>
       </section>
     </section>
   </main>
